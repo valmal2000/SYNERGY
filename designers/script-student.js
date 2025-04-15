6d257b37d7e15d7beac2f85caa3388f9ecafe876
tateridenour <tatejridenour@gmail.com>
2025-04-15 17:47:51 -0400

    Add files via upload

---
+++ designers/script-student.js
@@ -0,0 +1,2 @@
+const navbarLinksHeight = document.getElementById('navbar__links').getBoundingClientRect().height;
+document.getElementById('bio').style.paddingTop = (navbarLinksHeight + 40) + 'px';
